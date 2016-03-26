94385e37c70221247df183256ce7ac363293d292
shidongzhu <shidongzhu@172.19.198.74>
2016-03-26 16:33:04 +0800

    diyici

---
+++ src/edu/ahu/cst/zsd/controller/CustomerController.java
@@ -0,0 +1,8 @@
+package edu.ahu.cst.zsd.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CustomerController {
+
+}
---
+++ src/edu/ahu/cst/zsd/controller/ProductController.java
@@ -0,0 +1,5 @@
+package edu.ahu.cst.zsd.controller;
+
+public class ProductController {
+
+}
