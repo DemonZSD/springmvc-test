f8445b797dd49af501b3509c034a03cf6bf0a8e4
shidongzhu <shidongzhu@172.19.198.74>
2016-03-29 09:31:58 +0800

    删除spring mvc 配置文件 部分内容

--- src/edu/ahu/cst/zsd/controller/ProductController.java
+++ src/edu/ahu/cst/zsd/controller/ProductController.java
@@ -12,26 +12,26 @@ import edu.ahu.cst.zsd.form.ProductForm;
 @Controller
 public class ProductController {
 
-//	private static final Logger logger = Logger.getLogger(ProductController.class);
+	private static final Logger logger = Logger.getLogger(ProductController.class);
 	
-	@RequestMapping(value="/product_input")
-	public String inputProduct(){
+//	@RequestMapping(value="/product_input")
+//	public String inputProduct(){
 //		logger.info("调用了inputProduct方法");
-		return "ProductForm";
-	}
-	
-	@RequestMapping(value="/product_save")
-	public String saveProduct(ProductForm pf,Model model){
+//		return "ProductForm";
+//	}
+//	
+//	@RequestMapping(value="/product_save")
+//	public String saveProduct(ProductForm pf,Model model){
 //		logger.info("调用了saveProduct方法");
-		Product product = new Product();
-		product.setId(pf.getId());
-		product.setName(pf.getName());
-		try {
-			product.setPrice(Float.parseFloat(pf.getPrice()));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		model.addAttribute("product", product);
-		return "ProductDetails";
-	}
+//		Product product = new Product();
+//		product.setId(pf.getId());
+//		product.setName(pf.getName());
+//		try {
+//			product.setPrice(Float.parseFloat(pf.getPrice()));
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		model.addAttribute("product", product);
+//		return "ProductDetails";
+//	}
 }
